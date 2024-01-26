f4230b17f827bb1b8c3551398a4679682c72156f
Wyeben <yila.ben@yahoo.com>
2024-01-26 08:29:50 +0100

    added code

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -79,10 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tweeter_db',
-        'PASSWORD': 'password&123',
-        'USER': 'root',
 
-        'HOST': 'localhost'
     }
 }
 
