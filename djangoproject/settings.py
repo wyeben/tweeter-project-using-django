a85ff34c25ba026d8ad423d61d5e3a04f2230913
Wyeben <yila.ben@yahoo.com>
2024-01-27 09:05:48 +0100

    added code

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
