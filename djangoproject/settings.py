6306895c4d5af6444246de8a1b1bb1871e986965
Wyeben <yila.ben@yahoo.com>
2024-01-29 07:17:39 +0100

    added code

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+
     'django.contrib.staticfiles',
     'debug_toolbar',
     'tweet',
